88b631cc9ca17e1df0a25c879616b5cbe1f1a200
Parrot77 <christianestoque7@gmail.com>
2022-05-27 02:35:20 +0800

    Update script.js

--- assets/challenge-lists/faq-accordion/script.js
+++ assets/challenge-lists/faq-accordion/script.js
@@ -1,15 +1,27 @@
 const itemContainer = document.getElementsByClassName("item-container");
+const questions = document.getElementsByClassName("item-question");
+const boxIllustration = document.querySelector(".illustration-box");
 
 for (let i = 0; i < itemContainer.length; i++) {
-    itemContainer[i].addEventListener('click', () => {
-        console.log(itemContainer[i].classList.contains("active"));
+    questions[i].addEventListener('click', () => {
         if (itemContainer[i].classList.contains("active")) {
             itemContainer[i].classList.remove("active");
+            console.log("WHAT");
         } else {
             let activeElem = document.querySelector('.active');
             if (activeElem !== null) activeElem.classList.remove("active");
-            
+
             itemContainer[i].classList.add("active");
+            console.log("DAFAK");
         }
+    });
+}
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('mouseover', () => {
+        boxIllustration.classList.add("hovered");
+    });
+    questions[i].addEventListener('mouseout', () => {
+        boxIllustration.classList.remove("hovered");
     });
 }
