39d54a8ee05fcf6b727666badbd2c5714bc84366
Parrot77 <christianestoque7@gmail.com>
2022-05-27 03:36:38 +0800

    Final faq accordion card commit

--- assets/challenge-lists/faq-accordion/script.js
+++ assets/challenge-lists/faq-accordion/script.js
@@ -1,18 +1,17 @@
 const itemContainer = document.getElementsByClassName("item-container");
 const questions = document.getElementsByClassName("item-question");
 const boxIllustration = document.querySelector(".illustration-box");
+const optionArrow = document.querySelectorAll(".item-question img");
 
 for (let i = 0; i < itemContainer.length; i++) {
     questions[i].addEventListener('click', () => {
         if (itemContainer[i].classList.contains("active")) {
             itemContainer[i].classList.remove("active");
-            console.log("WHAT");
         } else {
             let activeElem = document.querySelector('.active');
             if (activeElem !== null) activeElem.classList.remove("active");
 
             itemContainer[i].classList.add("active");
-            console.log("DAFAK");
         }
     });
 }
