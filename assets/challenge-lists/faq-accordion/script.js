e1b9d5b0781a407b3e62b7bffbb38022f5a1af6f
Parrot77 <christianestoque7@gmail.com>
2022-05-27 02:04:28 +0800

    Update script.js

--- assets/challenge-lists/faq-accordion/script.js
+++ assets/challenge-lists/faq-accordion/script.js
@@ -1,48 +1,15 @@
-// const question = document.getElementsByClassName("item-question");
-// const answer = document.getElementsByClassName("item-answer");
 const itemContainer = document.getElementsByClassName("item-container");
-
-const showOrHide = (question) => {
-    let active = document.querySelector(".item-container.active");
-    console.log(question.classList.contains("active"));
-    if (active === question) {
-        question.classList.remove("active");
-        console.log("fag")
-    } else {
-        question.classList.add("active");
-    }
-
-
-    // console.log(active);
-    // console.log(question);
-}
 
 for (let i = 0; i < itemContainer.length; i++) {
     itemContainer[i].addEventListener('click', () => {
-        let activeElem = document.querySelector('.active');
-        if (activeElem !== null) activeElem.classList.remove("active");
-        showOrHide(itemContainer[i]);
-        // console.log("clicked");
+        console.log(itemContainer[i].classList.contains("active"));
+        if (itemContainer[i].classList.contains("active")) {
+            itemContainer[i].classList.remove("active");
+        } else {
+            let activeElem = document.querySelector('.active');
+            if (activeElem !== null) activeElem.classList.remove("active");
+            
+            itemContainer[i].classList.add("active");
+        }
     });
 }
-
-// for (let i = 0; i < question.length; i++) {
-//     question[i].addEventListener('click', () => {
-//         let activeElem = document.querySelectorAll('.active');
-//         for(let active of activeElem){
-//             console.log(active);
-//             if (active !== null) active.classList.remove("active");
-//         }
-//         question[i].classList.toggle("active");
-//         answer[i].classList.toggle("active");
-//         console.log("clicked");
-//     });
-// }
-// Array.from(question).forEach((question) => {
-//     question.addEventListener('click', () => {
-//         let activeElem = document.querySelector(".selected");
-//         if (activeElem !== null) activeElem.classList.remove("selected");
-//         question.classList.add("selected");
-//         selectedOption = option.innerHTML;
-//     });
-// });
