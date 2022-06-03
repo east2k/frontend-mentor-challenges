e2ce062927b5cc97c5484fe9c719edf9c0823e7b
Parrot77 <christianestoque7@gmail.com>
2022-06-03 23:23:01 +0800

    Added objects for project list

---
+++ script.js
@@ -0,0 +1,139 @@
+const projectList = [{
+        name: "Interactive Rating",
+        description: "Small project to practice handling user interactions and updating the DOM.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/interactive-rating.png",
+        liveLink: "challenge-lists/interactive-rating/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/interactive-rating"
+    }, {
+        name: "QR Code",
+        description: "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/qr-code.png",
+        liveLink: "challenge-lists/qr-code/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/qr-code"
+    }, {
+        name: "NFT Preview Code",
+        description: "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/nft-preview-card.png",
+        liveLink: "challenge-lists/nft-preview-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/nft-preview-card"
+    }, {
+        name: "Order Summary",
+        description: "A perfect project for newbies who are starting to build confidence with layouts.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/order-summary.png",
+        liveLink: "challenge-lists/order-summary/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/order-summary-component-QlPmajDUj",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/order-summary"
+    }, {
+        name: "Stats Preview Card",
+        description: "Small challenge to help get used to building to a design.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/stat-preview-card.png",
+        liveLink: "challenge-lists/stat-preview-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/stats-preview-card-component-8JqbgoU62",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/stats-preview-card"
+    }, {
+        name: "3-Column Preview Card",
+        description: "This challenge is perfect if you're just getting started. The shift between the layouts will be a nice test if you're new to building responsive projects.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/3-col-prev-card.png",
+        liveLink: "challenge-lists/3-col-prev-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/3-column-prev-card"
+    }, {
+        name: "Profile card",
+        description: "This is a perfect challenge to test your layout skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/profile-card.png",
+        liveLink: "challenge-lists/profile-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/profile-card-component-cfArpWshJ",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/profile-card"
+    }, {
+        name: "FAQ Accordion",
+        description: "This is an extremely common front-end pattern, so it's a great opportunity to get some practice in.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/faq-accordion-card.png",
+        liveLink: "challenge-lists/faq-accordion-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/faq-accordion"
+    }, {
+        name: "Social Proof Section",
+        description: "This project will test your layout skills. If you're starting to get confident with Flexbox or Grid, this will provide a nice challenge.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/social-proof-section.png",
+        liveLink: "challenge-lists/social-proof-section/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/social-proof-section-6e0qTv_bA",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/social-proof-section"
+    }, {
+        name: "Article Preview",
+        description: "Practice your layout skills with this article preview component. There's lots of fun to be had playing around with animations for the sharing icons as well.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/article-preview.png",
+        liveLink: "challenge-lists/article-preview/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/article-preview"
+    }, {
+        name: "Four Card Feature",
+        description: "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/four-card.png",
+        liveLink: "challenge-lists/four-card/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/four-card-feature"
+    }, {
+        name: "Base Apparel",
+        description: "This layout looks simple enough, but there are some interesting details to it that will test your CSS skills. You'll also get to practice basic form validation.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/base-apparel.png",
+        liveLink: "challenge-lists/base-apparel/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/base-apparel"
+    }, {
+        name: "Signup and Intro",
+        description: "Practice building out a sign-up form complete with client-side validation using JavaScript.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/intro-signup.png",
+        liveLink: "challenge-lists/intro-signup/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/intro-signup"
+    }, {
+        name: "Single Price Grid",
+        description: "In this challenge, you will build out the pricing component to the designs provided. This is perfect for beginners and people who want to complete a smaller challenge.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/single-price-grid.png",
+        liveLink: "challenge-lists/single-price-grid/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/single-price-grid"
+    }, {
+        name: "Coming Soon Page",
+        description: "This challenge is great for beginners and offers a chance to practice basic client-side form validation.",
+        tags: ["Newbie", "HTML", "CSS", "JS"],
+        image: "assets/thumbnails/ping-page.png",
+        liveLink: "challenge-lists/ping-page/index.html",
+        frontendLink: "www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/ping-coming-soon-page"
+    }, {
+        name: "Huddle Landing Page",
+        description: "A perfect challenge for beginners, this project will get you working with a two column layout.",
+        tags: ["Newbie", "HTML", "CSS"],
+        image: "assets/thumbnails/huddle-landing-page.png",
+        liveLink: "challenge-lists/huddle-landing-page/index.html",
+        frontendLink: "https://www.frontendmentor.io/challenges/huddle-landing-page-with-a-single-introductory-section-B_2Wvxgi0",
+        repositoryLink: "https://github.com/east2k/project-manager/tree/main/challenge-lists/huddle-landing-page"
+    }
+    // , {
+    //     name: "naaaaaaaaaaaaaaaaaaaaaaaaaaaaame",
+    //     description: "yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+    //     tags: ["Newbie", "HTML", "CSS"],
+    //     image: "assets/thumbnails/naaaaaaaaaaaaaaaaaaaaaaaaaaaaame.png",
+    //     liveLink: "challenge-lists/naaaaaaaaaaaaaaaaaaaaaaaaaaaaame/index.html",
+    //     frontendLink: "liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiink",
+    //     repositoryLink: "liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiink"
+    // }
+]
