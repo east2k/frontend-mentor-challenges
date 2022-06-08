720e35891bf4c42254bf14fb566fbede6ae54c24
Parrot77 <christianestoque7@gmail.com>
2022-06-08 16:11:29 +0800

    Polished filtering UI and script

--- filter-challenge.js
+++ filter-challenge.js
@@ -1,28 +1,25 @@
-const dropdown = document.getElementsByClassName("dropdown");
-const dropdownOptions = document.getElementsByClassName("dropdown-options");
+const dropdownContainer = document.querySelector(".dropdown-container");
+const dropdown = document.querySelector(".dropdown");
 
-// Dropdown toggle
-for (let i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener('click', () => {
-        dropdown[i].classList.toggle("expand");
-        clickOutside(i);
-
-    });
-}
+// Dropdown toggl
+dropdown.addEventListener('click', () => {
+    dropdownContainer.classList.toggle("expand");
+    clickOutside();
+});
 
 // When clicked outside dropdown
-const clickOutside = (i) => {
+const clickOutside = () => {
     document.addEventListener('click', (e) => {
-        if (!dropdown[i].contains(e.target)) {
-            dropdown[i].classList.remove("expand");
+        if (!dropdownContainer.contains(e.target)) {
+            dropdownContainer.classList.remove("expand");
         }
     });
 }
 
+// Checker if true or false
 let checker = (filter) => {
     const areFalseDifficulty = Object.values(filter.difficulty).every(value => value === false);
     const areFalseType = Object.values(filter.type).every(value => value === false);
-    // console.log(areFalseDifficulty && areFalseType)
     if (areFalseDifficulty && areFalseType) {
         addChallenge(challengeList)
     } else if (!areFalseDifficulty && areFalseType || areFalseDifficulty && !areFalseType) {
@@ -32,6 +29,7 @@ let checker = (filter) => {
     }
 };
 
+// Filter
 const filterChallenges = (challenge, filter, status) => {
     const filterKeys = Object.keys(filter);
     const difficultyKeys = Object.keys(filter.difficulty);
@@ -59,11 +57,9 @@ const filterChallenges = (challenge, filter, status) => {
         }
         if (!includeDiff && !includeType) include = false;
         if (status) {
-            if (!includeDiff && includeType) include = true;
-            else if (includeDiff && !includeType) include = true;
+            if ((!includeDiff && includeType) || (includeDiff && !includeType)) include = true;
         } else {
-            if (!includeDiff && includeType) include = false;
-            else if (includeDiff && !includeType) include = false;
+            if ((!includeDiff && includeType) || (includeDiff && !includeType)) include = false;
             else if (includeDiff || includeType) include = true;
         }
         return include;
@@ -87,7 +83,6 @@ const filters = {
 }
 
 // Update Filters depending on clicked
-
 const updateFilterObject = (event) => {
     const name = event.target.name;
     const value = event.target.value;
